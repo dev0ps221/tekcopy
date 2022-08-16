ef348b56657a4a048dca8d1bad6dac2fc51cee4a
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-16 08:07:39 +0000

    tekcopy

--- tekcopy.py
+++ tekcopy.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 
 def writeTo(file,lines):
-    file.writelines(lines)
+    try:
+        file.writelines(lines)
+        return True
+    except Exception as e:
+        print(e.__CAUSE__,' is an exception')
+        return False
 
 def readFrom(file):
-    return file.readLines()
+    return file.readlines()
 
 def copy(src,dst):
-    srcFile = open()
-    lines = readFrom() 
+    srcFile = open(src,'rb')
+    dstFile = open(dst,'ab')
+    lines = readFrom(srcFile)
+    return writeTo(dstFile,lines)
+    
+
+
+copy('tekcopy.py','tekcopy2.py') 
 
 
 
