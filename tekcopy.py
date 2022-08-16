9241a5aa8aaa8d126af50212078a27591dca6c3b
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-16 07:54:08 +0000

    tekcopy

---
+++ tekcopy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def writeTo(file,lines):
+    file.writelines(lines)
+
+def readFrom(file):
+    return file.readLines()
+
+def copy(src,dst):
+    srcFile = open()
+    lines = readFrom() 
+
+
+
+
